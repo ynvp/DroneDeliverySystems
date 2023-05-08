5330f954210211717bff589576db3b4f9460daba
Pradeep Yendluri <nagavarapradeepyendluri@gmail.com>
2023-05-08 16:47:54 -0500

    Delete research directory

--- research/removed_code.py
+++
@@ -1,3 +0,0 @@
-self.save_marker_button = tkinter.Button(master=self.listbox_button_frame, width=20, text="save current marker",
-                                         command=self.save_marker)
-self.save_marker_button.grid(row=0, column=0, pady=10, padx=10)
